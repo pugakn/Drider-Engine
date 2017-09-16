bf75b082d8a0a1e9991030078a7ffe1e9a4f1377
Francisco Abarca <idv15c.fjabarca@uartesdigitales.edu.mx>
2017-09-15 22:51:45 -0500

    Vector2D methods added.

--- Math/Include/dr_vector2d.h
+++ Math/Include/dr_vector2d.h
@@ -1,40 +1,179 @@
 #pragma once
 
-/*
-Delete this comment:
-
-Declaration Order:
-  Typedefs and enums
-  Constructors
-  Destructors
-  Methods, including static methods
-  Constants (static const data members)
-  Data members (Except static const data members)
-*/
+#include <cmath>
+#include <dr_prerequisites.h>
 
 namespace driderSDK {
-
-namespace math {
 
 class Vector2D
 {
  public:
-  Vector2D();
+  /**
+  * Default constructor
+  */
+
+  Vector2D()
+    : m_x(0.0f),
+      m_y(0.0f)
+  {};
+
+  /**
+  * Move constructor
+  *
+  */
+
   Vector2D(Vector2D && V) = default;
-  Vector2D(const Vector2D & V);
-  Vector2D(float _x, float _y);
-  virtual ~Vector2D();
+
+  /**
+  * Copy constructor
+  *
+  */
+
+  Vector2D(const Vector2D & V)
+    : m_x(V.m_x),
+      m_y(V.m_y)
+  {};
+
+  /**
+  * Initialize constructor with values.
+  *
+  * @param _x
+  * The x value of the vector
+  *
+  * @param _y
+  * The y value of the vector
+  */
+
+  Vector2D(Float32 x, Float32 y)
+    : m_x(x),
+      m_y(y)
+  {};
+
+  /**
+  * Default destructor
+  *
+  */
+
+  ~Vector2D()
+  {};
+
+  /**
+  * Computes the dot product of this vector and the vector parameter.
+  * This operatios is commutative.
+  *
+  * @param B
+  *  Right value of operation.
+  *
+  * @return
+  *   The sum of the products of the corresponding entries of the
+  *   vectors.
+  */
+
+  FORCEINLINE Float32
+  dot(const Vector2D B) const
+  {
+  return (m_x*B.m_x) + (m_y*B.m_y);
+  };
+
+  /**
+  * Computes the magnitude of this vector.
+  *
+  * @return
+  *   The magnitude (or "size") of the vector.
+  */
+
+  FORCEINLINE Float32
+  magnitude() const
+  {
+  return sqrt(dot(*this));
+  };
+
+  /**
+  * Get this vector normalized.
+  *
+  * @return
+  *   This vector normalized.
+  */
+
+  FORCEINLINE Vector2D
+  normalize() const
+  {
+  return (*this) / magnitude();
+  };
+
+  FORCEINLINE Vector2D
+  operator+ (const Vector2D & A) const
+  {
+  return Vector2D(m_x + A.m_x, m_y + A.m_y);
+  };
+  FORCEINLINE Vector2D&
+  operator+= (const Vector2D & A)
+  {
+  m_x += A.m_x;
+  m_y += A.m_y;
+  return *this;
+  };
+
+  FORCEINLINE Vector2D
+  operator- (const Vector2D & A) const
+  {
+  return Vector2D(m_x - A.m_x, m_y - A.m_y);
+  };
+  FORCEINLINE Vector2D&
+  operator-= (const Vector2D & A)
+  {
+  m_x -= A.m_x;
+  m_y -= A.m_y;
+  return *this;
+  };
+
+  FORCEINLINE Vector2D
+  operator* (const Vector2D & A) const
+  {
+  return Vector2D(m_x*A.m_x, m_y*A.m_y);
+  };
+  FORCEINLINE Vector2D&
+  operator*= (const Vector2D & A)
+  {
+  m_x *= A.m_x;
+  m_y *= A.m_y;
+  return *this;
+  };
+
+  FORCEINLINE Vector2D
+  operator* (const Float32 S) const
+  {
+    return Vector2D(m_x*S, m_y*S);
+  };
+  FORCEINLINE Vector2D&
+  operator*= (const Float32 S)
+  {
+    m_x *= S;
+    m_y *= S;
+    return *this;
+  };
+
+  FORCEINLINE Vector2D
+  operator/ (const Float32 S) const
+  {
+  return Vector2D(m_x, m_y);
+  };
+  FORCEINLINE Vector2D&
+  operator/= (const Float32 S)
+  {
+  m_x;
+  m_y;
+  return *this;
+  };
 
   union
   {
     struct
     {
-      float x, y;
+      Float32 m_x, m_y;
     };
-    float v[2];
+    Float32 v[2];
   };
 };
-
-}
 
 }
--- Math/Include/dr_vector2di.h
+++ Math/Include/dr_vector2di.h
@@ -1,5 +1,17 @@
 #pragma once
 
+/*
+Remove this comment:
+.h Include order:
+1. C library
+2. C++ Library
+3. Other libraries .h or .hpp
+4. Our project .h or .hpp
+*/
+
+#include <cstring>              //C Library
+#include <dr_prerequisites.h>   //Our project .h? or other library .h?
+
 namespace driderSDK {
 
 namespace math {
@@ -10,16 +22,16 @@ class Vector2DI
   Vector2DI();
   Vector2DI(Vector2DI && V) = default;
   Vector2DI(const Vector2DI & V);
-  Vector2DI(__int32 _x, __int32 _y);
+  Vector2DI(Int32 _x, Int32 _y);
   virtual ~Vector2DI();
 
   union
   {
     struct
     {
-      __int32 x, y;
+      Int32 x, y;
     };
-    __int32 v[2];
+    Int32 v[2];
   };
 };
 
