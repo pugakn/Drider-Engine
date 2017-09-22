e6259d71c84bf5ae3f692306c659fe6022ada939
montionproductions <idv15c.jmontion@uartesdigitales.edu.mx>
2017-09-22 13:09:55 -0500

    Aggregated dr_collisions

--- Math/Include/dr_sphere.h
+++ Math/Include/dr_sphere.h
@@ -11,6 +11,12 @@ class DR_API_EXPORT Sphere
  public:
   Sphere();
   //Sphere(Float32 radio, Vector3D position);
+  Float32 GetRadio();
+  Vector3D GetPosition();
+
+  //void SetRadio();
+  //void SetPosition();
+
  protected:
  private:
   Float32 m_radio;
--- Math/Source/dr_sphere.cpp
+++ Math/Source/dr_sphere.cpp
@@ -7,4 +7,14 @@ Sphere::Sphere()
   
 }
 
+Float32 Sphere::GetRadio()
+{
+  return m_radio;
+}
+
+Vector3D Sphere::GetPosition()
+{
+  return m_position;
+}
+
 }
