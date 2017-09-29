7a4c61ff63719d21fe83936ceaa64859aa5d3069
montionproductions <idv15c.jmontion@uartesdigitales.edu.mx>
2017-09-28 19:48:49 -0500

    Capsule class

--- Math/Include/dr_capsule.h
+++ Math/Include/dr_capsule.h
@@ -1,10 +1,25 @@
 #pragma once
+#include <dr_prerequisites.h>
+#include "dr_vector3d.h"
 
-namespace driderSDKSDK {
-  
-class Capsule
+namespace driderSDK {
+
+class Plane;
+class Sphere;
+class Capsule;
+class Frustrum;
+class Ray;
+
+class DR_API_EXPORT Capsule
 {
  public:
+
+  Capsule()
+  {}
+
+  Vector3D pointA;
+  Vector3D pointB;
+  float radio;
  protected:
  private:
 };
--- Math/Include/dr_sphere.h
+++ Math/Include/dr_sphere.h
@@ -2,37 +2,21 @@
 
 #include <dr_prerequisites.h>
 #include "dr_vector3d.h"
-
 namespace driderSDK {
 
 class DR_API_EXPORT Sphere
 {
-
  public:
   //Uncommented
-  Sphere();
-
-  //Uncommented
-  FORCEINLINE float 
-  getRadio() const
-  {
-   return m_radio;
-  }
-  
-  //Uncommented
-  FORCEINLINE Vector3D
-  getCenter() const
-  {
-   return m_center;
-  }
-
+  Sphere()
+  {}
+  float m_radio;
+  Vector3D m_center;
   //void SetRadio();
   //void SetPosition();
 
  protected:
  private:
-  float m_radio;
-  Vector3D m_center;
 
 };
 
