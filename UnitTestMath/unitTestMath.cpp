694305fcd25b96c0afb40b6f8e4cfc6a56048270
montionproductions <idv15c.jmontion@uartesdigitales.edu.mx>
2017-10-17 20:30:16 -0500

    dr_unitTest_sphere

--- UnitTestMath/unitTestMath.cpp
+++ UnitTestMath/unitTestMath.cpp
@@ -15,35 +15,9 @@ bool recev(int& asin)
 int main(int argc, char* argv[])
 {
   ::testing::InitGoogleTest(&argc, argv);
+  ::testing::GTEST_FLAG(filter) = "*.Sphere";
   RUN_ALL_TESTS();
-  driderSDK::Matrix4x4 lu(1, 4, -1, 0, 2, 3, 5, -2, 0, 3, 1, 6, 3, 0, 2, 1);
 
-  driderSDK::Radian dckRad(5);
-  dckRad = dckRad + 3;
-  dckRad = -dckRad;
-  float lucas = 10;
-  float lucas2 = 11;
-
-  lucas -= lucas2;
-
-  float po = lu.determinant();
-  //lu.cofactor();
-  //lu.adjugate();
-  lu.inverse();
-
-  driderSDK::VectorN<4> mlg;
-  auto vk = mlg.length();
-
-  driderSDK::Vector3D pointA(0.0f, 0.0f, 0.0f);
-  driderSDK::Vector3D pointB(2.0f, 2.0f, 0.0f);
-  driderSDK::Vector3D pointC(0.0f, 1.0f, 0.0f);
-
-  driderSDK::Vector3D dirAB = pointB - pointA;
-  driderSDK::Vector3D dirAC = pointC - pointA;
-
-  driderSDK::Plane plane({ 0, -1, 1 }, 3);
-  plane.normalize();
-  auto distance = plane.distanceToPoint({ 1,3,4 });
 
   return 0;
 }
