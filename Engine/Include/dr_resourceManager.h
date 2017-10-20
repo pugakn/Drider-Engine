505380036152c41c7e9c52798717ac447148ddb2
montionproductions <idv15c.jmontion@uartesdigitales.edu.mx>
2017-10-20 17:48:22 -0500

    dr_face, dr_polygon, dr_resource, dr_resourceManager

---
+++ Engine/Include/dr_resourceManager.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <dr_prerequisites.h>
+
+namespace driderSDK {
+
+class DR_API_EXPORT ResourceManager {
+public:
+  ResourceManager() {}
+  virtual ~ResourceManager() {}
+
+
+};
+
+}
---
+++ Engine/Source/dr_resourceManager.cpp
@@ -0,0 +1,1 @@
+#include "dr_resourceManager.h"
