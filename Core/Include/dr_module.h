d3280ec0f1bb28161a0698172cff310d43ee5a35
Francisco Abarca <idv15c.fjabarca@uartesdigitales.edu.mx>
2017-10-20 15:03:57 -0500

    Module working, example of use in UnitTestMath

--- Core/Include/dr_module.h
+++ Core/Include/dr_module.h
@@ -2,66 +2,34 @@
 #include <dr_prerequisites.h>
 
 namespace driderSDK {
-/**
-* Represents one engine module. Essentially it is a specialized type
-* of singleton. Module must be manually started up and shut down
-* before and after use.
-*
-*/
+
 template<class T>
 class Module
 {
- public:
-  /**
-  * @return
-  *   A reference to the module instance. Module has to have
-  *   been started up first otherwise an exception will be thrown.
-  */
-  static T&
-  instance() {
+public:
+  static T& instance() {
     if (isShutDown()) {
-      return nullptr;
-      //dr_EXCEPT(InternalErrorException, "Trying to access a module but it hasn't been started.");
-    }
-    if (isDestroyed()) {
-      return nullptr;
-      //DR_EXCEPT(InternalErrorException, "Trying to access a destroyed module.");
+      throw "Trying to access a module but it hasn't been started.";
     }
 
+    if (isDestroyed()) {
+      throw "Trying to access a destroyed module.";
+    }
     return *_instance();
   }
-
-  /**
-  * @return
-  *   A pointer to the module instance. Module has to have been
-  *   started up first otherwise an exception will be thrown.
-  *
-  */
-  static T*
-  instancePtr() {
+  static T* instancePtr() {
     if (isShutDown()) {
-      return nullptr;
-      //DR_EXCEPT(InternalErrorException, "Trying to access a module but it hasn't been started.");
+      throw "Trying to access a module but it hasn't been started.";
     }
 
     if (isDestroyed()) {
-      return nullptr;
-      //GE_EXCEPT(InternalErrorException, "Trying to access a destroyed module.");
+      throw "Trying to access a destroyed module.";
     }
-
     return _instance();
   }
-
-  /**
-  * Constructs and starts the module using the specified parameters.
-  *
-  */
-  template<class... Args>
-  static void
-  startUp(Args&& ...args) {
+  template<class... Args> static void startUp(Args&& ...args) {
     if (!isShutDown()) {
-      //GE_EXCEPT(InternalErrorException, "Trying to start an already started module.");
-      return;
+      throw "Trying to start an already started module.";
     }
 
     _instance() = new T(std::forward<Args>(args)...);
@@ -69,19 +37,12 @@ class Module
 
     static_cast<Module*>(_instance())->onStartUp();
   }
-
-  /**
-  * Constructs and starts a specialized type of the module.
-  * Provided type must derive from type the Module is initialized with.
-  *
-  */
-  template<class SubType, class... Args>
-  static void
-  startUp(Args&& ...args) {
-    //DR_ASSERT(std::is_base_of<T, SubType>::value);
+  template<class SubType, class... Args> static void startUp(Args&& ...args) {
+    static_assert(std::is_base_of<T, SubType>::value,
+      "Provided type isn't derived from type the Module is initialized with.");
 
     if (!isShutDown()) {
-      //GE_EXCEPT(InternalErrorException, "Trying to start an already started module.");
+      throw "Trying to start an already started module.";
     }
 
     _instance() = new SubType(std::forward<Args>(args)...);
@@ -89,15 +50,9 @@ class Module
 
     static_cast<Module*>(_instance())->onStartUp();
   }
-
-  /**
-  * Shuts down this module and frees any resources it is using.
-  *
-  */
-  static void
-  shutDown() {
+  static void shutDown() {
     if (isShutDown() || isDestroyed()) {
-      //GE_EXCEPT(InternalErrorException, "Trying to shut down an already shut down module.");
+      throw "Trying to shut down an already shut down module.";
     }
 
     static_cast<Module*>(_instance())->onShutDown();
@@ -105,100 +60,31 @@ class Module
     delete(_instance());
     isShutDown() = true;
   }
-
-  /**
-  * Query if the module has been started.
-  *
-  */
-  static bool
-  isStarted() {
+  static bool isStarted() {
     return !isShutDown() && !isDestroyed();
   }
-
- protected:
-  /**
-  * Default constructor.
-  *
-  */
+protected:
   Module() {}
-
-  /**
-  * Default constructor (virtual).
-  *
-  */
-  virtual
-  ~Module() {
+  virtual ~Module() {
     _instance() = nullptr;
     isDestroyed() = true;
   }
-
-  /**
-  * Default copy constructor.
-  *
-  */
   Module(const Module&) {}
-
   Module& operator=(const Module&) {
     return *this;
   }
 
-  /**
-  * @brief
-  *   Override if you want your module to be notified once it has been
-  *   constructed and started.
-  *
-  * @note
-  *   Useful when your module is polymorphic and you cannot perform some
-  *   implementation specific initialization in constructor itself.
-  *
-  */
-  virtual void
-  onStartUp() {}
-
-  /**
-  * @brief
-  *   Override if you want your module to be notified just before it is deleted.
-  *
-  * @note
-  *   Useful when your module is polymorphic and you might want to perform
-  *   some kind of clean up perhaps overriding that of a base class.
-  *
-  */
-  virtual void
-  onShutDown() {}
-
-  /**
-  * @brief
-  *   Returns a singleton instance of this module. Throws an exception
-  *   if module is not yet initialized.
-  */
-  static T*&
-  _instance() {
-  static T* inst = nullptr;
+  virtual void onStartUp() {}
+  virtual void onShutDown() {}
+  static T*& _instance() {
+    static T* inst = nullptr;
     return inst;
   }
-
-  /**
-  * @brief
-  *   Checks has the Module been shut down.
-  *
-  * @note
-  *   If module was never even started, this will return false.
-  *
-  */
-  static bool&
-  isDestroyed() {
+  static bool& isDestroyed() {
     static bool inst = false;
     return inst;
   }
-
-  /**
-  * @brief
-  *   Checks has the Module been started up.
-  *
-  */
-  static bool&
-  isShutDown() {
+  static bool& isShutDown() {
     static bool inst = true;
     return inst;
   }
--- MathUnitTest/Source/unitTestMath.cpp
+++ MathUnitTest/Source/unitTestMath.cpp
@@ -1,19 +1,26 @@
 #include <gtest\gtest.h>
 #include <dr_logger.h>
 
-extern driderSDK::Module<driderSDK::Logger> ModuleLogger;
-/*
 void yourWaifuIsShit() {
-  driderSDK::Logger* theNameIsATruth = ModuleLogger.instancePtr();
-  theNameIsATruth->addWarning(__FILE__, __LINE__, "I Am Error");
+  driderSDK::Logger ModuleLogger;
+  if (!ModuleLogger.isStarted()) {
+    ModuleLogger.startUp();
+  }
+  driderSDK::Logger& htmlLogger = ModuleLogger.instance();
+  htmlLogger.addWarning(__FILE__, __LINE__, "your waifu is shit");
 }
-*/
 
 int main(int argc, char* argv[])
 {
-  driderSDK::Logger* mainLogger = ModuleLogger.instancePtr();
-  mainLogger->reset();
-  mainLogger->addWarning(__FILE__, __LINE__, "A warning sample");
+  driderSDK::Logger ModuleLogger;
+  if (!ModuleLogger.isStarted()) {
+    ModuleLogger.startUp();
+  }
+  driderSDK::Logger& htmlLogger = ModuleLogger.instance();
+  htmlLogger.reset();
+  htmlLogger.addError(__FILE__, __LINE__, "An error example");
+
+  yourWaifuIsShit();
 
   ::testing::GTEST_FLAG(output) = "xml";
   ::testing::InitGoogleTest(&argc, argv);
