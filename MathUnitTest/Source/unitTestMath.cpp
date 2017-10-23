f58277c78b26154a13b4a4b3c9c44916362fdb6e
Francisco Abarca <idv15c.fjabarca@uartesdigitales.edu.mx>
2017-10-23 17:40:05 -0500

    Modified unitTestMath to run the gridTest

--- MathUnitTest/Source/unitTestMath.cpp
+++ MathUnitTest/Source/unitTestMath.cpp
@@ -24,11 +24,6 @@ int main(int argc, char* argv[])
 
   ::testing::GTEST_FLAG(output) = "xml";
   ::testing::InitGoogleTest(&argc, argv);
-  //::testing::GTEST_FLAG(filter) = "Vector4D.*";
-  //::testing::GTEST_FLAG(filter) = "Radian.*";
-  //::testing::GTEST_FLAG(filter) = "Vector3D.*";
-  //::testing::GTEST_FLAG(filter) = "Matrix3x3.*";
-	//::testing::GTEST_FLAG(filter) = "aabb.*";
   RUN_ALL_TESTS();
 
   return 0;
