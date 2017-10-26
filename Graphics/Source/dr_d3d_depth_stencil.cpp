fa6a7eec5ae70e754de22edf8d017a0478a988c7
Francisco Abarca <idv15c.fjabarca@uartesdigitales.edu.mx>
2017-10-26 18:39:28 -0500

    Styling *.cpp's (2/2)

--- Graphics/Source/dr_d3d_depth_stencil.cpp
+++ Graphics/Source/dr_d3d_depth_stencil.cpp
@@ -4,29 +4,38 @@
 #include "dr_d3d_device.h"
 #include "dr_d3d_device_context.h"
 #include "dr_d3d_texture.h"
+
 namespace driderSDK {
-  DR_GRAPHICS_ERROR::E D3DDepthStencil::create(const Device & device, const DrTextureDesc & desc)
-  {
-    depthTexture = std::make_unique<D3DTexture>();
-    DR_GRAPHICS_ERROR::E DRErr;
-    DRErr = depthTexture->createEmpty(device, desc);
-    if (DRErr != DR_GRAPHICS_ERROR::ERROR_NONE)
-    {
-      return DRErr;
-    }
-    D3D11_DEPTH_STENCIL_VIEW_DESC dDesc;
-    ZeroMemory(&dDesc, sizeof(D3D11_DEPTH_STENCIL_VIEW_DESC));
-    dDesc.Format = static_cast<DXGI_FORMAT>(desc.Format);
-    dDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2DMS;//Hardcoded
-    if (static_cast<const D3DDevice*>(&device)->D3D11Device->CreateDepthStencilView(static_cast<D3DTexture*>(depthTexture.get())->APITexture.Get(), &dDesc, &APIDepthView) != S_OK)
-    {
-      return DR_GRAPHICS_ERROR::CREATE_RESOURCE_VIEW_ERROR;
-    }
-    return DR_GRAPHICS_ERROR::ERROR_NONE;
+
+DR_GRAPHICS_ERROR::E D3DDepthStencil::create(const Device& device,
+                                             const DrTextureDesc& desc) {
+  depthTexture = std::make_unique<D3DTexture>();
+  DR_GRAPHICS_ERROR::E DRErr;
+  DRErr = depthTexture->createEmpty(device, desc);
+
+  if (DRErr != DR_GRAPHICS_ERROR::ERROR_NONE) {
+    return DRErr;
   }
-  void D3DDepthStencil::release()
-  {
-    depthTexture.release();
-    APIDepthView.Reset();
+
+  D3D11_DEPTH_STENCIL_VIEW_DESC dDesc;
+  ZeroMemory(&dDesc, sizeof(D3D11_DEPTH_STENCIL_VIEW_DESC));
+  dDesc.Format = static_cast<DXGI_FORMAT>(desc.Format);
+  dDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2DMS;//Hardcoded
+
+  if (static_cast<const D3DDevice*>(&device)->
+        D3D11Device->
+          CreateDepthStencilView(static_cast<D3DTexture*>(depthTexture.get())->
+                                   APITexture.Get(),
+                                 &dDesc, &APIDepthView) != S_OK) {
+    return DR_GRAPHICS_ERROR::CREATE_RESOURCE_VIEW_ERROR;
   }
+  return DR_GRAPHICS_ERROR::ERROR_NONE;
+}
+
+void
+D3DDepthStencil::release() {
+  depthTexture.release();
+  APIDepthView.Reset();
+}
+
 }
--- Graphics/Source/dr_d3d_input_layout.cpp
+++ Graphics/Source/dr_d3d_input_layout.cpp
@@ -4,31 +4,43 @@
 #include <vector>
 #include "dr_d3d_device.h"
 #include "dr_d3d_device_context.h"
+
 namespace driderSDK {
-  DR_GRAPHICS_ERROR::E D3DInputLayout::create(const Device & device, const DrInputElementDesc* inputDescArray, UInt32 arraySize)
-  {
-    std::vector<D3D11_INPUT_ELEMENT_DESC> desc;
-    for (size_t i = 0; i < arraySize; i++)
-    {
-      desc[i].Format = (DXGI_FORMAT)inputDescArray[i].format;
-      desc[i].AlignedByteOffset = inputDescArray[i].offset;
-      desc[i].SemanticIndex = inputDescArray[i].semanticIndex;
-      desc[i].SemanticName = inputDescArray[i].semanticName;
-      desc[i].SemanticIndex = inputDescArray[i].inputSlot;
-    }
+
+DR_GRAPHICS_ERROR::E
+D3DInputLayout::create(const Device& device,
+                       const DrInputElementDesc* inputDescArray,
+                       UInt32 arraySize) {
+  std::vector<D3D11_INPUT_ELEMENT_DESC> desc;
+
+  for (size_t i = 0; i < arraySize; i++) {
+    desc[i].Format = (DXGI_FORMAT)inputDescArray[i].format;
+    desc[i].AlignedByteOffset = inputDescArray[i].offset;
+    desc[i].SemanticIndex = inputDescArray[i].semanticIndex;
+    desc[i].SemanticName = inputDescArray[i].semanticName;
+    desc[i].SemanticIndex = inputDescArray[i].inputSlot;
+  }
    
-    if (static_cast<const D3DDevice*>(&device)->D3D11Device->CreateInputLayout(&desc[0], arraySize, 0, 0, APILayout.GetAddressOf()) != S_OK)
-    {
-      return DR_GRAPHICS_ERROR::CREATE_INPUT_LAYOUT_ERROR;
-    }
-    return DR_GRAPHICS_ERROR::ERROR_NONE;
+  if (static_cast<const D3DDevice*>(&device)->
+        D3D11Device->
+          CreateInputLayout(&desc[0], arraySize, 0, 0, APILayout.GetAddressOf()) != S_OK) {
+    return DR_GRAPHICS_ERROR::CREATE_INPUT_LAYOUT_ERROR;
   }
-  void D3DInputLayout::set(const DeviceContext & deviceContext) const
-  {
-    static_cast<const D3DDeviceContext*>(&deviceContext)->D3D11DeviceContext->IASetInputLayout(APILayout.Get());
-  }
-  void D3DInputLayout::release()
-  {
-    APILayout.Reset();
-  }
+
+  return DR_GRAPHICS_ERROR::ERROR_NONE;
+}
+
+void
+D3DInputLayout::set(const DeviceContext & deviceContext) const {
+  static_cast<const D3DDeviceContext*>(&deviceContext)->
+    D3D11DeviceContext->
+      IASetInputLayout(APILayout.Get());
+}
+
+void
+D3DInputLayout::release()
+{
+  APILayout.Reset();
+}
+
 }
