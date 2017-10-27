9c66b81b80d8d5a22819f22279b4e673f4ceb36f
Francisco Abarca <idv15c.fjabarca@uartesdigitales.edu.mx>
2017-10-27 00:20:54 -0500

    Graphics project fully documented (or not? :doubtingDuck:)

--- Graphics/Include/dr_d3d_input_layout.h
+++ Graphics/Include/dr_d3d_input_layout.h
@@ -10,14 +10,39 @@ namespace driderSDK {
 class DR_API_EXPORT D3DInputLayout : public InputLayout
 {
  public:
+   /**
+   * Create a input layout
+   *
+   * @param device
+   *   An array of DrInputElementDesc that describes the input layout parameters
+   *
+   * @param arraySize
+   *   The size of the input desc array
+   *
+   * @param out layout
+   *   The InputLayout object taht will be filled out
+   *
+   * @return
+   *   Return a DR_GRAPHICS_ERROR code, ERROR_NONE means all went well
+   */
   DR_GRAPHICS_ERROR::E
   create(const Device& device,
          const DrInputElementDesc* inputDescArray,
          UInt32 arraySize) override;
 
+  /**
+  * Set the input layout
+  *
+  * @param deviceContext
+  *   The device context to set the layout
+  *
+  */
   void
   set(const DeviceContext& deviceContext)const override;
 
+  /**
+  * Release the allocated memory
+  */
   void
   release() override;
 
--- Graphics/Include/dr_d3d_vertex_shader.h
+++ Graphics/Include/dr_d3d_vertex_shader.h
@@ -7,15 +7,46 @@ struct ID3D11VertexShader;
 
 namespace driderSDK {
 
+/**
+* Class for vertex shader
+*
+* Sample usage:
+*	VertexShader* = new D3DVertexShader;
+*/
 class DR_API_EXPORT D3DVertexShader : public VertexShader
 {
  public:
+  /**
+  * Set the shader to the stage
+  *
+  * @param deviceContext
+  *   The device context to set the shader
+  *
+  */
   void
   set(const DeviceContext& deviceContext) const override;
 
+  /**
+  * Release the allocated memory
+  */
   void
   release() override;
 
+  /**
+  * Update the buffer with new data
+  *
+  * @param deviceContext
+  *   The device context to get acces to the resource
+  *
+  * @param buffer
+  *   The uncompiled shader buffer
+  *
+  * @param bufferSize
+  *   The uncompiled shader buffer size
+  *
+  * @return
+  *   Return a DR_GRAPHICS_ERROR code, ERROR_NONE means all went well
+  */
   DR_GRAPHICS_ERROR::E
   createFromMemory(const Device& device, const char* buffer, size_t bufferSize) override;
 
