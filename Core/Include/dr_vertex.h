bd9a58b5d4e550e9cde6231ca144fddbb88a7007
Gerardo Pérez <geras_amerik4@hotmail.com>
2017-11-13 20:11:51 -0600

    Added DRIDER_DEPENDENCIES include

--- Core/Include/dr_vertex.h
+++ Core/Include/dr_vertex.h
@@ -1,7 +1,6 @@
 #pragma once
 
 namespace driderSDK {
-
   class Vertex 
   {
     
--- Engine/Include/dr_codecTexture.h
+++ Engine/Include/dr_codecTexture.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "dr_engine_prerequisites.h"
 #define STB_IMAGE_IMPLEMENTATION
-#include <STB\stb-master\stb_image.h>
+#include <STB\stb_image.h>
 #include <comdef.h>
 
 #include "dr_imageInfo.h"
