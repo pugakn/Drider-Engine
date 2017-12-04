5dea648dafaf5103f26c1fbcbec64571db754bac
Francisco Abarca <idv15c.fjabarca@uartesdigitales.edu.mx>
2017-12-04 17:17:01 -0600

    Added OS specific functions

--- Engine/Source/dr_file_system_linux.cpp
+++ Engine/Source/dr_file_system_linux.cpp
@@ -1,5 +1,6 @@
-#if DR_PLATFORM == DR_PLATFORM_LINUX  
 #include "dr_file_system.h"
+#if DR_PLATFORM == DR_PLATFORM_LINUX
+#include <unistd.h>
 
 namespace driderSDK
 {
@@ -7,6 +8,19 @@ namespace driderSDK
 TString
 FileSystem::GetWorkingPath() {
   TString fullpath;
+  ANSIChar cCurrentPath[FILENAME_MAX];
+
+  if (!getcwd(cCurrentPath, sizeof(cCurrentPath))) {
+    return fullpath;
+  }
+
+  cCurrentPath[sizeof(cCurrentPath) - 1] = '\0';
+
+  SizeT curChar = 0;
+  while (curChar < sizeof(cCurrentPath)) {
+    fullpath += cCurrentPath[curChar];
+    ++curChar;
+  }
 
   return fullpath;
 }
