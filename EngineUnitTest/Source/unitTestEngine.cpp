a43d4f7a8e7b02919ad719bb3701a0e74df81263
Francisco Abarca <idv15c.fjabarca@uartesdigitales.edu.mx>
2017-12-06 01:30:10 -0600

    Gtest of File and FileSystem ready

--- EngineUnitTest/Source/unitTestEngine.cpp
+++ EngineUnitTest/Source/unitTestEngine.cpp
@@ -5,7 +5,9 @@ int main(int argc, char* argv[])
   ::testing::GTEST_FLAG(output) = "xml";
   ::testing::InitGoogleTest(&argc, argv);
   //::testing::GTEST_FLAG(filter) = "ModelResource.*";
-  ::testing::GTEST_FLAG(filter) = "FileSystem.*";
+  //::testing::GTEST_FLAG(filter) = "FileSystem.*";
+  ::testing::GTEST_FLAG(filter) = "File.*";
   RUN_ALL_TESTS();
+
   return 0;
 }
