8c1e57370c5759b15e97433dcaa0262167594513
Francisco Abarca <idv15c.fjabarca@uartesdigitales.edu.mx>
2017-12-05 22:53:39 -0600

    Started File and File system GTests

--- EngineUnitTest/Source/unitTestEngine.cpp
+++ EngineUnitTest/Source/unitTestEngine.cpp
@@ -4,7 +4,8 @@ int main(int argc, char* argv[])
 {
   ::testing::GTEST_FLAG(output) = "xml";
   ::testing::InitGoogleTest(&argc, argv);
-  ::testing::GTEST_FLAG(filter) = "ModelResource.*";
+  //::testing::GTEST_FLAG(filter) = "ModelResource.*";
+  ::testing::GTEST_FLAG(filter) = "FileSystem.*";
   RUN_ALL_TESTS();
   return 0;
 }
