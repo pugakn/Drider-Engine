214776bdd116643123c4c4c3d808590ef5287aaf
Ansaor04 <ansaor04@hotmail.com>
2017-12-06 12:32:34 -0600

    Fixed unitTest_camaraManager tests

--- CoreUnitTest/Source/dr_unitTest_cameraManager.cpp
+++ CoreUnitTest/Source/dr_unitTest_cameraManager.cpp
@@ -15,49 +15,52 @@ TEST(CameraManager, constructor)
 
 TEST(CameraManager, destructor)
 {
-	EXPECT_TRUE(false);
+	driderSDK::CameraManager Manager;
+	driderSDK::CameraManager* pManager = nullptr;
+	pManager = &Manager;
+	EXPECT_TRUE(&pManager->getActiveCamera() == nullptr);
 }
 
 TEST(CameraManager, createCamera)
 {
-	driderSDK::CameraManager pManager;
-	pManager.createCamera("test", driderSDK::Vector3D(0, 0, 0), driderSDK::Vector3D(0, 0, 1), driderSDK::Viewport{ 0,0,900,600,0,1 }, 90, 0.1f, 1000);
-	pManager.setActiveCamera("test");
-	EXPECT_EQ(pManager.getActiveCamera().getName(), "test");
+	driderSDK::CameraManager Manager;
+	Manager.createCamera("test", driderSDK::Vector3D(0, 0, 0), driderSDK::Vector3D(0, 0, 1), driderSDK::Viewport{ 0,0,900,600,0,1 }, 90, 0.1f, 1000);
+	Manager.setActiveCamera("test");
+	EXPECT_EQ(Manager.getActiveCamera().getName(), "test");
 }
 
 TEST(CameraManager, deleteCamera)
 {
-	driderSDK::CameraManager pManager;
-	pManager.createCamera("test", driderSDK::Vector3D(0, 0, 0), driderSDK::Vector3D(0, 0, 1), driderSDK::Viewport{ 0,0,900,600,0,1 }, 90, 0.1f, 1000);
-	pManager.setActiveCamera("test");
-	pManager.deleteCamera("test");
-	EXPECT_TRUE(&pManager.getActiveCamera() == nullptr);
+	driderSDK::CameraManager Manager;
+	Manager.createCamera("test", driderSDK::Vector3D(0, 0, 0), driderSDK::Vector3D(0, 0, 1), driderSDK::Viewport{ 0,0,900,600,0,1 }, 90, 0.1f, 1000);
+	Manager.setActiveCamera("test");
+	Manager.deleteCamera("test");
+	EXPECT_TRUE(&Manager.getActiveCamera() == nullptr);
 }
 
 TEST(CameraManager, setViewportToCamera)
 {
-	driderSDK::CameraManager pManager;
+	driderSDK::CameraManager Manager;
 	driderSDK::Viewport viewport{0,0,1024,720,0,1};
-	pManager.createCamera("test", driderSDK::Vector3D(0, 0, 0), driderSDK::Vector3D(0, 0, 1), driderSDK::Viewport{ 0,0,900,600,0,1 }, 90, 0.1f, 1000);
-	pManager.setActiveCamera("test");
-	pManager.setViewportToCamera("test", viewport);
-	EXPECT_EQ(pManager.getActiveCamera().m_viewport.width, 1024);
+	Manager.createCamera("test", driderSDK::Vector3D(0, 0, 0), driderSDK::Vector3D(0, 0, 1), driderSDK::Viewport{ 0,0,900,600,0,1 }, 90, 0.1f, 1000);
+	Manager.setActiveCamera("test");
+	Manager.setViewportToCamera("test", viewport);
+	EXPECT_EQ(Manager.getActiveCamera().m_viewport.width, 1024);
 }
 
 TEST(CameraManager, getActiveCamera)
 {
-	driderSDK::CameraManager pManager;
-	pManager.createCamera("test", driderSDK::Vector3D(0, 0, 0), driderSDK::Vector3D(0, 0, 1), driderSDK::Viewport{ 0,0,900,600,0,1 }, 90, 0.1f, 1000);
-	pManager.setActiveCamera("test");
-	EXPECT_EQ(pManager.getActiveCamera().getName(), "test");
+	driderSDK::CameraManager Manager;
+	Manager.createCamera("test", driderSDK::Vector3D(0, 0, 0), driderSDK::Vector3D(0, 0, 1), driderSDK::Viewport{ 0,0,900,600,0,1 }, 90, 0.1f, 1000);
+	Manager.setActiveCamera("test");
+	EXPECT_EQ(Manager.getActiveCamera().getName(), "test");
 }
 
 TEST(CameraManager, setActiveCamera)
 {
-	driderSDK::CameraManager pManager;
-	pManager.createCamera("test", driderSDK::Vector3D(0, 0, 0), driderSDK::Vector3D(0, 0, 1), driderSDK::Viewport{ 0,0,900,600,0,1 }, 90, 0.1f, 1000);
-	pManager.setActiveCamera("test");
-	EXPECT_EQ(pManager.getActiveCamera().getName(), "test");
+	driderSDK::CameraManager Manager;
+	Manager.createCamera("test", driderSDK::Vector3D(0, 0, 0), driderSDK::Vector3D(0, 0, 1), driderSDK::Viewport{ 0,0,900,600,0,1 }, 90, 0.1f, 1000);
+	Manager.setActiveCamera("test");
+	EXPECT_EQ(Manager.getActiveCamera().getName(), "test");
 }
 
