2fecdd9544eb9389f8a9329052ba1004d81a52e0
Francisco Abarca <idv15c.fjabarca@uartesdigitales.edu.mx>
2018-01-31 10:28:25 -0600

    Writed in a comment the render order

--- RenderManager/Source/dr_rendermanapp.cpp
+++ RenderManager/Source/dr_rendermanapp.cpp
@@ -5,8 +5,24 @@ namespace driderSDK {
 
 void
 renderManagerApp() {
-  do {
+  RenderPass renderAdmin;
 
+  do {
+  /*
+  Render order:
+  Normal + Depth Pre-Pass
+  Shadow Maps
+  Screen Space Ambient Occlusion
+  Light Pre-Pass
+  Forward-Rendering of Opaque Objects
+  Transparent Objects
+  Light Bloom
+  Anti-Aliasing
+  Color Correction
+  User Interface
+
+  Depth of Field
+  */
   } while(true);
 }
 
