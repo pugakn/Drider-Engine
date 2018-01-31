0189bbff12c76b9314ab5c0e4bdf157157eb8ddc
Francisco Abarca <idv15c.fjabarca@uartesdigitales.edu.mx>
2018-01-31 16:33:42 -0600

    Solved some cppcheck warnings

--- GraphicsTestApplication/Source/Model3D.cpp
+++ GraphicsTestApplication/Source/Model3D.cpp
@@ -12,11 +12,11 @@
 
 using namespace driderSDK;
 
-Model3D::Model3D()
-{}
+Model3D::Model3D() : CB(nullptr), vs(nullptr), fs(nullptr), IL(nullptr), elapsedTime(0.0f) {
+}
 
-void Model3D::init(Device & device, const TString& filename) {
-
+void
+Model3D::init(Device& device, const TString& filename) {
   elapsedTime = 0.0f;
 
   std::unique_ptr<Codec> cod = dr_make_unique<CodecModel>();
@@ -47,10 +47,12 @@ void Model3D::init(Device & device, const TString& filename) {
   file.Close();
 
   vs = reinterpret_cast<Shader*>(device.createShaderFromMemory(vsSource.c_str(), 
-                                       vsSource.size() + 1,DR_SHADER_TYPE_FLAG::kVertex));
+                                                               vsSource.size() + 1,
+                                                               DR_SHADER_TYPE_FLAG::kVertex));
 
   fs = reinterpret_cast<Shader*>(device.createShaderFromMemory(fsSource.c_str(),
-                                         fsSource.size() + 1 , DR_SHADER_TYPE_FLAG::kFragment));
+                                                               fsSource.size() + 1,
+                                                               DR_SHADER_TYPE_FLAG::kFragment));
 
 
   std::vector<DrInputElementDesc> idesc;
@@ -72,18 +74,23 @@ void Model3D::init(Device & device, const TString& filename) {
     bdesc.type = DR_BUFFER_TYPE::kVERTEX;
     bdesc.sizeInBytes = mesh.vertices.size() * sizeof(Vertex);
     bdesc.stride = sizeof(Vertex);
-    VB = reinterpret_cast<VertexBuffer*>(device.createBuffer(bdesc, reinterpret_cast<driderSDK::byte*>(mesh.vertices.data())));
+    VB = reinterpret_cast<VertexBuffer*>(device.createBuffer(bdesc,
+                                                             reinterpret_cast<driderSDK::byte*>
+                                                               (mesh.vertices.data())));
   
     bdesc.type = DR_BUFFER_TYPE::kINDEX;
     bdesc.sizeInBytes = mesh.indices.size() * sizeof(UInt32);
     bdesc.stride = 0;
-    IB = reinterpret_cast<IndexBuffer*>(device.createBuffer(bdesc, reinterpret_cast<driderSDK::byte*>(mesh.indices.data())));
+    IB = reinterpret_cast<IndexBuffer*>(device.createBuffer(bdesc,
+                                                            reinterpret_cast<driderSDK::byte*>
+                                                              (mesh.indices.data())));
 
     meshesGFX.push_back({VB, IB});
   }
 }
 
-void Model3D::destroy() {
+void
+Model3D::destroy() {
   CB->release();
 
   for (auto& mesh : meshesGFX) {
@@ -95,13 +102,14 @@ void Model3D::destroy() {
   vs->release();
 }
 
-void Model3D::update() {
+void
+Model3D::update() {
   elapsedTime += 0.007f;
   animator.evaluate(elapsedTime);
 }
 
-void Model3D::draw(const driderSDK::DeviceContext& deviceContext, const Camera& camera) {
-  
+void
+Model3D::draw(const driderSDK::DeviceContext& deviceContext, const Camera& camera) {
   auto wvp = transform.getTransformMatrix() * camera.getVP();
   
   auto& boneTransforms = animator.getTransforms();
