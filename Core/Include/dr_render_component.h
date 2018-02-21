9ec622c720201889fcb12f1d9e7468627f90ccf3
Gerardo Pérez <geras_amerik4@hotmail.com>
2018-02-21 16:57:45 -0600

    Added render component

---
+++ Core/Include/dr_render_component.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <vector>
+#include "dr_core_prerequisites.h"
+#include "dr_gameComponent.h"
+#include "dr_vertex.h"
+
+namespace driderSDK {
+
+struct MaterialRenderInfo
+{
+  Int32 indexStart;
+  Int32 indexSize;
+  Int32 vertexStart;
+  Int32 vertexSize;
+};
+
+class RenderComponent : public GameComponent
+{
+public:
+
+  using IndexBuffer = std::vector<Int32>;
+  using VertexBuffer = std::vector<Vertex>;
+  using MaterialList = std::vector<MaterialRenderInfo>;
+
+  RenderComponent(GameObject& _gameObject,
+                  IndexBuffer&& _indexBuffer, 
+                  VertexBuffer&& _vertexBuffer,
+                  MaterialList&& _materials);
+
+  const IndexBuffer&
+  getIndexBuffer()
+  {
+    return m_indexBuffer;
+  }
+
+  const VertexBuffer&
+  getVertexBuffer()
+  {
+    return m_vertexBuffer;
+  }
+
+  const MaterialList&
+  getMaterials()
+  {
+    return m_materials;
+  }
+
+private:
+  IndexBuffer m_indexBuffer;
+  VertexBuffer m_vertexBuffer;
+  MaterialList m_materials;
+};
+
+}
