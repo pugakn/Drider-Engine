19c00e36c692bdad760828f3dfda5630b38b1fda
Gerardo Pérez <patos.50@hotmail.com>
2018-02-27 12:31:13 -0600

    Changed Graphics API members to Smart Ptr Changed Graphics API get Methods to static Added dr_gfx_deleter to dr_memory Added dr_unique_custom to dr_memory for custom deleters

--- Graphics/Source/dr_graphics_driver.cpp
+++ Graphics/Source/dr_graphics_driver.cpp
@@ -1,35 +1,39 @@
 #include "dr_graphics_driver.h"
 namespace driderSDK {
-GraphicsDriver::GraphicsDriver(DR_GRAPHICS_API::E apiType, UInt32 w, UInt32 h, void * hwnd)
+GraphicsDriver::GraphicsDriver(DR_GRAPHICS_API::E apiType, 
+                               UInt32 w, 
+                               UInt32 h, 
+                               void * hwnd)
 {
   switch (apiType)
   {
   case DR_GRAPHICS_API::D3D11:
-    m_api = new D3DGraphicsAPI;
+    m_api = dr_make_unique<D3DGraphicsAPI>();
     break;
   }
+
   m_api->init(w, h, hwnd);
 }
 GraphicsDriver::~GraphicsDriver()
-{
+{}
+
+GraphicsAPI* 
+GraphicsDriver::APIPtr() {
+  return instance().m_api.get();
 }
-GraphicsAPI * GraphicsDriver::getApiPointer()
-{
-  return instance().m_api;
+
+GraphicsAPI& 
+GraphicsDriver::API() {
+  return *APIPtr();
 }
-GraphicsAPI& GraphicsDriver::getApiReference()
-{
-  return *instance().m_api;
-}
-void GraphicsDriver::changeApi()
-{
-}
+
+void GraphicsDriver::changeApi(DR_GRAPHICS_API::E) {}
+
 void GraphicsDriver::onStartUp()
-{
-}
+{}
+
 void GraphicsDriver::onShutDown()
 {
-  delete m_api;
-  m_api = nullptr;
+  m_api->destroy();
 }
 }
