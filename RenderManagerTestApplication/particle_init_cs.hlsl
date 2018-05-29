2abc772351a5626fd30a0fcf61ecb1ef7e11cbca
Francisco Abarca <idv15c.fjabarca@uartesdigitales.edu.mx>
2018-05-29 17:02:36 -0500

    Added dependencies to RenderManager and ParticleSystem, removed unnecesary libs in ParticleSystem

--- RenderManagerTestApplication/particle_init_cs.hlsl
+++ RenderManagerTestApplication/particle_init_cs.hlsl
@@ -8,7 +8,7 @@ struct PoolBuffer
   float lifeTime;
   float scale;
   int isActive;
-  float speedLimit;
+  int padaaasdasdas;
 };
 RWStructuredBuffer<PoolBuffer> poolBuffer : register(u0);
 AppendStructuredBuffer<uint> DeadBuffer : register(u1);
@@ -18,7 +18,7 @@ void CS( uint3 id : SV_DispatchThreadID )
 {
   DeadBuffer.Append(id.x);
   poolBuffer[id.x].isActive = 0;
-  poolBuffer[id.x].lifeTime = 0;
+  poolBuffer[id.x].lifeTime = 3.402823466e+38F;
   poolBuffer[id.x].color = float4(1,0,1,1);
   poolBuffer[id.x].velocity = float4(0, 0, 0, 0);
   poolBuffer[id.x].acceleration = float4(0, 0, 0, 0);
--- RenderManagerTestApplication/particle_ps.hlsl
+++ RenderManagerTestApplication/particle_ps.hlsl
@@ -1,21 +1,8 @@
-#define DR_PARTICLES_CPU
-
 struct PS_INPUT {
   float4 Position : SV_POSITION;
   float4 color : COLOR;
 };
-
-#ifdef DR_PARTICLES_GPU
-struct BufType
-{
-  float4 color;
-};
-
-StructuredBuffer<BufType> BufferOut : register(t0);
-#endif
 float4
 FS(PS_INPUT input) : SV_TARGET0 {
-#ifdef DR_PARTICLES_CPU
   return float4(input.color.xyz, 1.0f);
-#endif
 }
