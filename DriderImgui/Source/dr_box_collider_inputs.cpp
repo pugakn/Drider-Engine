419e9c5166c9033190f225aa7c9cd405d2aae3c7
montionproductions <idv15c.jmontion@uartesdigitales.edu.mx>
2018-12-20 19:15:16 -0600

    Box and Sphere input

--- DriderImgui/Source/dr_box_collider_inputs.cpp
+++ DriderImgui/Source/dr_box_collider_inputs.cpp
@@ -1,10 +1,19 @@
 #include "dr_box_collider_inputs.h"
 
+#include <dr_box_collider.h>
+#include <dr_collision_body.h>
+
 namespace driderSDK {
 
 void
 BoxColliderInputs::getInputs() {
-  ImGui::Text("Hello BoxColliderInputs!");
+  auto& boxComponent = static_cast<BoxCollider&>(m_component);
+  if (ImGui::CollapsingHeader(StringUtils::toString(boxComponent.getName()).c_str())) {
+
+    //AABB
+    ImGui::TextDisabled("Box(1.0, 1.0, 1.0)");
+    
+  }
 }
 
 }
